f853959741fc8cf68e16a584c8096245020ea851
iamzwq <876867644@qq.com>
2021-12-31 16:35:11 +0800

    optimization

--- src/stores/home.ts
+++ src/stores/home.ts
@@ -1,9 +1,11 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import type { RootStore } from ".";
+import { fetchData } from "@/services/home";
 
 class HomeStore {
   rootStore: RootStore;
   count = 0;
+  list: any[] = [];
 
   constructor(rootStore: RootStore) {
     makeAutoObservable(this, { rootStore: false });
@@ -16,6 +18,19 @@ class HomeStore {
 
   decrease() {
     this.count--;
+  }
+
+  async getData() {
+    try {
+      const res = await fetchData({});
+      if (res) {
+        runInAction(() => {
+          this.list = res.data || [];
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
